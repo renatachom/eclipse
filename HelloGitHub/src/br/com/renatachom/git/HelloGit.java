2b4c6d9542c1be0c60bf7fe45c836063b098aee6
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-01-09 10:43:55 -0300

    Adicionei arquivos via upload

---
+++ HelloGitHub/src/br/com/renatachom/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.renatachom.git;
+
+/**
+ * @author Renata Chaves
+ * Exemplo de integração do Eclipse com GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+
+	}
+
+}
